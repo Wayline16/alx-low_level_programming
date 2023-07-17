6597536869cc71ed3e58e46942dd3f53ea617a6c
Wayline <jeffrieswayline@gmail.com>
2023-07-17 05:05:55 -0700

    3. Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+
+#endif
+/**
+ * Write a function-like macro ABS(x)
+ * that computes the absolute value of a number x.
+ */
