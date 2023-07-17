01fe18f84ea22986d6605b0ac22f8fa3ce88dab7
Wayline <jeffrieswayline@gmail.com>
2023-07-17 04:58:57 -0700

    1. Pi

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,10 @@
+#ifndef PI_H
+#define PI_H
+
+#define PI 3.14159265359
+
+#endif
+/**
+ * Create a header file that defines a macro named
+ * PI as an abbreviation for the token 3.14159265359.
+ */
