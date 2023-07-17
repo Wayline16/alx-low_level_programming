2b5e8d261abc26c7a2252638df585df7a4c2f51a
Wayline <jeffrieswayline@gmail.com>
2023-07-17 04:52:03 -0700

    0. Object-like Macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,9 +1,9 @@
-/**
- * Create a header file that defines a macro named
- * SIZE as an abbreviation for the token 1024.
- */
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 #define SIZE 1024
 
 #endif
+/**
+ * Create a header file that defines a macro named
+ * SIZE as an abbreviation for the token 1024.
+ */
