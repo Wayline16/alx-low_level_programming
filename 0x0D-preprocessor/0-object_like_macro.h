faf16b4bbf86c6d06d3467ef38a1754c49fcd264
Wayline Jeffries <120781316+Wayline16@users.noreply.github.com>
2023-08-17 11:09:00 +0200

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,9 +0,0 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-#define SIZE 1024
-
-#endif
-/**
- * Create a header file that defines a macro named
- * SIZE as an abbreviation for the token 1024.
- */
